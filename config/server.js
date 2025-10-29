42994ebd27f208230eb1b027470a04a9ef0137ee
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2025-10-29 21:54:19 +0100

    [FIxed values]

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   proxy: true,
-  url: env('PUBLIC_URL', 'https://m3dlab.it'),
+  url: env('PUBLIC_URL', 'https://m3dlab.onrender.com/'),
   app: {
     keys: env.array('APP_KEYS'),
   },
