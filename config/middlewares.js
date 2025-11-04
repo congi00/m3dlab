ff930f3883f82f22756601296afeafd4c1a9475d
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2025-11-04 19:43:58 +0100

    [Middleware update]

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,13 +1,25 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'global::log-proxy',
 ];
--- config/server.js
+++ config/server.js
@@ -1,14 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://m3dlab.onrender.com/'),
-  proxy: true,
+  url: env('PUBLIC_URL', 'https://m3dlab.onrender.com'),
+  proxy: true, // <-- fondamentale
+
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
