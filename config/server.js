63ee1f6046e1a984ffa031806b94419756ed0fed
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2025-11-04 17:46:34 +0100

    Fix proxy trust for HTTPS cookies on Render

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
   url: env('PUBLIC_URL', 'https://m3dlab.onrender.com/'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
