27b66e32eae8f8ca795d5a6361fc23494d0d0716
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2025-11-05 18:37:34 +0100

    [Fixed middleware]

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
-
+  register({ strapi }) {
+    // Force the socket to be treated as encrypted for proxy setups
+    strapi.server.use(async (ctx, next) => {
+      if (ctx.req?.socket) {
+        (ctx.req.socket).encrypted = true;
+      }
+      await next();
+    });
+  },
   /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
