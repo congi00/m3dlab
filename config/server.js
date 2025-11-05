a482baf38c1ce0a00f0e056591ff2a0c00b01d5a
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2025-11-05 15:18:15 +0100

    [changes]

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://m3dlab.onrender.com'),
-  proxy: true, // <-- fondamentale
-
+  url: env('PUBLIC_URL', 'https://3dmlab.up.railway.app'),
+  proxy: true,
+  trustProxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
