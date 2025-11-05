d6ef894105f8edef02e5d9c8aa1dd1cbb9009636
A.Congiusti.esterno <alessandrocongiusti@yahoo.it>
2025-11-05 18:00:02 +0100

    Fixed force https

--- src/middlewares/force-https.js
+++ src/middlewares/force-https.js
@@ -1,8 +1,15 @@
+'use strict';
+
 module.exports = (config, { strapi }) => {
-    return async (ctx, next) => {
-      // ✅ forza la richiesta a essere considerata sicura
-      ctx.request.secure = true;
-      await next();
-    };
+  return async (ctx, next) => {
+    // Forza la richiesta a essere considerata HTTPS
+    ctx.request.secure = true;
+
+    // Se serve, puoi anche impostare l'header X-Forwarded-Proto
+    if (!ctx.request.header['x-forwarded-proto']) {
+      ctx.request.header['x-forwarded-proto'] = 'https';
+    }
+
+    await next();
   };
-  
+};
